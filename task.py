e57e95ea2253ebc82ab4ae658b7007bef9eafdd4
mohanamit1222 <165678067+mohanamit1222@users.noreply.github.com>
2024-04-08 16:04:24 +0530

    weather checking project

---
+++ task.py
@@ -0,0 +1,26 @@
+import requests
+
+def get_weather(city, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    
+    if data["cod"] == 200:
+        weather_description = data["weather"][0]["description"]
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        wind_speed = data["wind"]["speed"]
+        
+        print(f"Weather in {city}:")
+        print(f"Description: {weather_description}")
+        print(f"Temperature: {temperature}°C")
+        print(f"Humidity: {humidity}%")
+        print(f"Wind Speed: {wind_speed} m/s")
+    else:
+        print("City not found.")
+
+if __name__ == "__main__":
+    
+    api_key = "e76b8078e30cf9b9361e168ad404ae3d"
+    city = input(f"Enter city name: ")
+    get_weather(city, api_key)
